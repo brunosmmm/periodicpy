980dd1acabd3489c0eaff93a065ee9952801b4da
brunosmmm <brunosmmm@gmail.com>
2016-03-10 15:46:39 -0500

    fixed key regex

--- wifitools/iwparse.py
+++ wifitools/iwparse.py
@@ -4,7 +4,7 @@ from periodicpy.wifitools.wifiinfo import WifiInfo
 BSS_REGEX = re.compile(r"^BSS\s([0-9A-Fa-f:]+).*")
 SSID_REGEX = re.compile(r"\s*SSID:\s([a-zA-Z0-9_-]+).*")
 SIGNAL_REGEX = re.compile(r"\s*signal:\s(-?[0-9]+\.[0-9]+)\sdBm.*")
-KEY_REGEX = re.compile(r"\s*Encription\skey:\s((yes|no)).*")
+KEY_REGEX = re.compile(r"\s*Encription\skey:\s*((yes|no)).*")
 
 class IWListParser(object):
 
