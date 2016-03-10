097d62aa6314b1be74e95bbc9db4e1c032f44702
brunosmmm <brunosmmm@gmail.com>
2016-03-10 14:18:06 -0500

    remove irrelevant code

--- wifitools/scan.py
+++ wifitools/scan.py
@@ -1,9 +1,7 @@
 import subprocess
 import re
 from periodicpy.wifitools.iwparse import IWListParser
-from periodicpy.wifitools.wifiinfo import check_interface_up, set_interface_up, IFaceError, WifiInfoEncoder
-import json
-import time
+from periodicpy.wifitools.wifiinfo import check_interface_up, set_interface_up, IFaceError
 
 class ScanError(Exception):
     pass
@@ -35,12 +33,3 @@ def scan_and_parse(interface_name, auto_up=False):
     iw_list = IWListParser.parse_text(out.split('\n'))
 
     return iw_list
-
-def scan_and_report():
-
-    while True:
-        scan_results = scan_and_parse('wlan0', True)
-        scan_json = open('/var/lib/periodicpi/wifi_scan.json', 'w')
-        scan_json.write(json.dumps(scan_results, cls=WifiInfoEncoder))
-        scan_json.close()
-        time.sleep(10)
